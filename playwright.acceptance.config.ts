4ac642e4c95c4e1582af60bd863bcb262b828980
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-04-29 10:54:46 +0200

    tests(E2E, Acceptance): use Docker Container

--- playwright.acceptance.config.ts
+++ playwright.acceptance.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: process.env.CI ? 'npm run start:docker' : 'npm run dev:docker',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI
   }
