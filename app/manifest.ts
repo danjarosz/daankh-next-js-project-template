367914553fd95d72cf99f12770708cc7816d044c
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-07-30 16:58:27 +0200

    update

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
+    name: 'Daankh Next.js Project Template',
+    short_name: 'Daankh NextPWA',
     description: 'A Progressive Web App built with Next.js',
     start_url: '/',
     display: 'standalone',
--- app/page.tsx
+++ app/page.tsx
@@ -136,66 +136,65 @@ function InstallPrompt() {
 
 export default function Page() {
   return (
-    <div>
-      <PushNotificationManager />
-      <InstallPrompt />
-    </div>
+    <>
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+      </head>
+      <div className={'flex flex-col items-center justify-center'}>
+        <div className={'flex items-center justify-center'}>
+          <div className={'border-2'}>
+            <PushNotificationManager />
+          </div>
+          <div className={'border-2'}>
+            <InstallPrompt />
+          </div>
+        </div>
+        <div className={'flex flex-col items-center justify-center'}>
+          <div className="flex flex-col items-center justify-center w-full lg:w-1/2">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <h1 className={''}>Daankh Next.js Project Template</h1>
+              </div>
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <Image
+                  className="dark:invert"
+                  src="/logo.svg"
+                  alt="Daankh Next.js Project Template"
+                  width={180}
+                  height={180}
+                  priority
+                />
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
-//
-// export default function Home() {
-//   return (
-//     <>
-//       <head>
-//         <link
-//           rel="icon"
-//           type="image/png"
-//           href="/favicon-96x96.png"
-//           sizes="96x96"
-//         />
-//         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-//         <link rel="shortcut icon" href="/favicon.ico" />
-//         <link
-//           rel="apple-touch-icon"
-//           sizes="180x180"
-//           href="/apple-touch-icon.png"
-//         />
-//       </head>
-//       <div className={'flex flex-col items-center justify-center'}>
-//         {/*<PushNotificationManager />*/}
-//         {/*<InstallPrompt />*/}
-//         <div className="flex flex-col items-center justify-center w-full lg:w-1/2">
-//           <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-//             <div
-//               style={{
-//                 width: '100%',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 justifyContent: 'center'
-//               }}
-//             >
-//               <h1 className={''}>Daankh Next.js Project Template</h1>
-//             </div>
-//             <div
-//               style={{
-//                 width: '100%',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 justifyContent: 'center'
-//               }}
-//             >
-//               <Image
-//                 className="dark:invert"
-//                 src="/logo.svg"
-//                 alt="Daankh Next.js Project Template"
-//                 width={180}
-//                 height={180}
-//                 priority
-//               />
-//             </div>
-//           </main>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
